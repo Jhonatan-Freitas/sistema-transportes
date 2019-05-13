6677246d93fb1cb8e19685612fc9b9e461d00a02
Jhonatan Freitas <itjhonatan01@gmail.com>
2019-05-13 19:11:37 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Alfa Desenvolvimento</title>
+		<!-- Página responsiva -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Bootstrap CSS -->
+	  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+		<!-- CSS desta página -->
+		<link rel="stylesheet" href="css/css.css">
+	</head>
+	<body>
+		<a name="topo"></a>
+		<header class="container">
+			<div class="row">
+				<div class="col-md-3 col-sm-6">
+					<a href="index.html"><img src="img/logo.png" style="float: left;"></a>
+				</div>
+				<div class="col-md-9">
+					<a href="index2.html"></a>
+				</div>
+			</div>
+		</header>	
+			<!--Menu Principal-->
+			<div class="container menuprincipal">
+					<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-end ">
+			    		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mNav">
+			      				<span class="navbar-toggler-icon"></span>
+			    		</button>
+			    		<div class="collapse navbar-collapse justify-content-end" id="mNav">
+			      			<ul class="navbar-nav">
+			        			<li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+			       			 	<li class="nav-item"><a class="nav-link" href="#sobre">Sobre</a></li>
+				    			<li class="nav-item"><a class="nav-link" href="#funcionario">Funcionários</a></li>
+				    			<li class="nav-item"><a class="nav-link" href="#contato">Contato</a></li>
+			      			</ul>
+			    		</div>
+			  		</nav>
+	  		</div>
+            <!--Fim do menu principal-->
+            <!--Sobre-->
+	  		<div class="container">
+	  			<div class="row">
+	  				<div class="col-md-4 col-12"><img class="img-fluid img-thumbnail mt-4 text-center" src="http://www.mariaaugusta.com.br/wp-content/uploads/2017/10/imagem-post-13-por-sfio-cracho-1030x661.jpg" style="width: 400px; float: left;"></div>
+	  				<div class="col-md-8 col-12"><p class="text-center mt-3 mb-3 lead"><b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit. Sed eu sagittis ipsum, a volutpat enim. Proin id facilisis erat. Proin dignissim, lectus vitae tristique lobortis, tortor erat dictum mi, vel rutrum ex lacus sed urna. Maecenas in sapien velit. Nam et laoreet sapien. Nunc ultricies, eros sit amet maximus interdum, mauris velit interdum est, et semper enim est at magna. Nulla rhoncus, libero eget finibus aliquet, diam leo efficitur diam, eu tristique urna lorem nec orci. Duis sed sapien a metus vestibulum condimentum in quis ante. Suspendisse nibh ipsum, ultrices eget feugiat vel, dapibus eget nunc. Suspendisse blandit elit metus, vitae feugiat metus iaculis eu. Quisque ipsum lorem, fermentum nec lorem vel, vulputate venenatis urna. Etiam eu lacus augue.</p></div>
+	  			
+      		    </div>
+                <!--Fim do Sobre-->
+      		    <hr>
+                <!--Mini conteudo-->
+      		    <div class="container">
+                      <div class="row">
+                        <div class="col-md-8 col-12"><p class="text-center mt-3 mb-3 lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu sagittis ipsum, a volutpat enim. Proin id facilisis erat. Proin dignissim, lectus vitae tristique lobortis, tortor erat dictum mi, vel rutrum ex lacus sed urna. Maecenas in sapien velit. Nam et laoreet sapien. Nunc ultricies, eros sit amet maximus interdum, mauris velit interdum est, et semper enim est at magna. Nulla rhoncus, libero eget finibus aliquet, diam leo efficitur diam, eu tristique urna lorem nec orci. Duis sed sapien a metus vestibulum condimentum in quis ante. Suspendisse nibh ipsum, ultrices eget feugiat vel, dapibus eget nunc.</p></div>
+                        <div class="col-md-4 col-12"><img class="img-fluid img-thumbnail mt-4 text-center" src="http://www.cetecambiental.eco.br/wp-content/uploads/NEG%C3%93CIOS-SUSTENT%C3%81VEIS.jpg" style="width: 400px; float: right;"></div>
+                    </div>
+                    <hr>
+      		    </div>
+                <!--Fim do mini conteudo-->
+                <!--Quem somos-->
+      		    <div class="container">
+      			   <div class="row">
+                        <div class="col-md-12 col-12"><h2 class="display-4 text-center mt-4" id="sobre">Quem Somos</h2>
+                            <p class="text-center lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu sagittis ipsum, a volutpat enim. Proin id facilisis erat. Proin dignissim, lectus vitae tristique lobortis, tortor erat dictum mi, vel rutrum ex lacus sed urna. Maecenas in sapien velit. Nam et laoreet sapien. Nunc ultricies, eros sit amet maximus interdum, mauris velit interdum est, et semper enim est at magna. Nulla rhoncus, libero eget finibus aliquet, diam leo efficitur diam, eu tristique urna lorem nec orci. Duis sed sapien a metus vestibulum condimentum in quis ante. Suspendisse nibh ipsum, ultrices eget feugiat vel, dapibus eget nunc.</p>
+
+                        </div>
+                        <div class="col-md-4 col-12">
+                            <img class="img-fluid img-thumbnail mt-5 mb-5 text-center" src="http://www.fabricadevalores.com.br/imagens/informacoes/consultoria-gestao-negocios-02.jpg" style="width: 400px;">
+                        </div>
+                        <div class="col-md-4 col-12">
+                            <img class="img-fluid img-thumbnail mt-4 text-center" src="https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/2O0VXeG/business-people-clapping-after-meeting-with-manager-in-conference-room_n3oohzodx__F0000.png" style="width: 400px;">
+                        </div>
+                        <div class="col-md-4 col-12">
+                            <img class="img-fluid img-thumbnail mt-5 mb-5 text-center" src="http://www.fumec.br/blog/wp-content/uploads/2016/04/como-funciona-o-curso-de-negocios-internacionais15082.jpg" style="width: 400px;">
+                        </div>
+
+      			   </div>
+      		    </div>
+                <!--Fim do quem somos-->
+                <!--Tabela nossos funcionários-->
+      		    <hr>
+      		    <div class="container"><h2 class="text-center display-4 mt-5" id="funcionario">Nossos       Funcionários</h2>
+      			   <table class="table  table-light table-responsive-sm mb-5 mt-5">
+                          <thead class="table-dark">
+                            <tr>
+                              <th>Nome</th>
+                              <th>E-mail</th>
+                              <th>Telefone</th>
+                            </tr>
+                          </thead>
+                            <tr>
+                              <td>Jhonatan Freitas</td>
+                              <td>itjhonatan01@gmail.com</td>
+                              <td>97151-6955</td>
+                            </tr>
+                            <tr>
+                              <td>Joaquim Rocha</td>
+                              <td>joaqRocha@brino.com</td>
+                              <td>9762-8422</td>
+                            </tr>
+                            <tr>
+                              <td>Maria Ceciliana</td>
+                              <td>marizinha@gmail.com</td>
+                              <td>7169-2406</td>
+                            </tr>
+		          </table>
+      		</div>
+                <!--Fim da tabela-->  
+      		<hr>
+                <!--Formulário de contato-->
+      		<div class="container">
+      			<h1 class="text-center display-4 mt-5 " id="contato"> Faça Contato</h1>
+                       <p class="lead text-center">Preencha o formulário abaixo para enviar uma mensagem para o site.</p>
+                        <form method="get" action="#">
+                              <div class="form-group">
+                                    <label for="nome">Nome:</label>
+                                    <input class="form-control" type="text" name="nome" id="nome">
+                               </div>
+                               <div class="form-group">
+                                     <label for="email">E-mail:</label>
+                                     <input class="form-control" type="email" name="email" id="email">
+                               </div>
+                               <div class="form-group">
+                                     <label for="mensagem">Mensagem:</label>
+                                     <textarea class="form-control" rows="4" name="mensagem" id="nome"></textarea>
+                                </div>
+                                <div class="form-group text-center">
+                                     <label></label>                        
+                                     <input class="btn btn-dark mb-5" type="submit" value="Enviar">
+                                </div>
+                          </form> 
+      		</div>	
+                <!--Fim do formulario-->
+	  			<!--Inicio do Rodapé-->
+	  			<footer class="container">
+	  				<div class="row">
+		  				<div class="col-md-4 bg-dark p-2 text-center"><a href="index.html"><p class="text-white"> Home</p></a></div>
+						<div class="col-md-4 bg-dark p-2 text-center"><p class="text-white"> &copy; Copyright 2018 Alfa Desenvolvimento</p></div>
+						<div class="col-md-4 bg-dark p-2 text-center"><a href="#topo"><p class="text-white"> Topo</p></a></div>
+					</div>
+				</footer>	
+	  		</div>	
+            <!--Fim do rodape-->
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+		<!-- JavaScript desta página -->
+		<script src="js/js.js"></script>
+
+	</body>
+</html>
