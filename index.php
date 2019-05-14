baf7d6efabcd0ffcd23fc2bdf4224a58c627c9d5
Jhonatan Freitas <itjhonatan01@gmail.com>
2019-05-13 21:13:39 -0300

    chamando o formulario

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 			       			 	<li class="nav-item"><a class="nav-link" href="#sobre">Sobre</a></li>
 				    			<li class="nav-item"><a class="nav-link" href="#funcionario">Funcionários</a></li>
 				    			<li class="nav-item"><a class="nav-link" href="#contato">Contato</a></li>
-								<li class="nav-item"><a class="nav-link" href="login.php">Entrar</a></li>
+								<li class="nav-item"><a class="nav-link" href="clienteFormulario.php">Entrar</a></li>
 			      			</ul>
 			    		</div>
 			  		</nav>
